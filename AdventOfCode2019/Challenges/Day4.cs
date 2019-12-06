204f5ca690ff8dc637788307bb997e3240a944bf
thiseas.matth <thiseas.matth@outlook.com>
2019-12-07 01:04:04 +0200

    Complete Day 4

--- AdventOfCode2019/Challenges/Day4.cs
+++ AdventOfCode2019/Challenges/Day4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -15,13 +16,19 @@ namespace AdventOfCode2019.Challenges
         {
             var passwordRange = ReadSv("Day4.txt", '-');
             var passwordCount = 0;
+            var validCount = 0;
 
             for (var index = passwordRange[0]; index <= passwordRange[1]; index++)
             {
-                if (CompareWithPattern(index))
-                    passwordCount++;
+                if (!CompareWithPattern(index)) continue;
+
+                passwordCount++;
+                if (!CompareWithStricterPattern(index))
+                    validCount++;
+
             }
-            Console.WriteLine($"The number of password matching the required pattern is: {passwordCount}.");
+            Console.WriteLine($"The number of passwords matching the required pattern is: {passwordCount}.");
+            Console.WriteLine($"The number of passwords containing only two consecutive values is: {validCount}.");
         }
 
         private static bool CompareWithPattern(int index)
@@ -31,6 +38,21 @@ namespace AdventOfCode2019.Challenges
 
             var password = index.ToString();
             return Regex.IsMatch(password, consecutiveRegex) && Regex.IsMatch(password, ascendingRegex);
+        }
+
+        private static bool CompareWithStricterPattern(int value)
+        {
+            const string threeOrMoreRegex = @"(.)\1\1{1,}";
+            var password = value.ToString();
+            var matches = Regex.IsMatch(password, threeOrMoreRegex);
+            if (!matches) return false;
+
+            if (password.Any(digit => password.Count(x => x == digit) == 2))
+            {
+                matches = false;
+            }
+
+            return matches;
         }
     }
 }
